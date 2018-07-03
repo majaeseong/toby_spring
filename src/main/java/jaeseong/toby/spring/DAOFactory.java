c34320ec970e1f2142ff09e2d437bd5fcf3b67a4
jaeseong <cpontina@naver.com>
2018-07-03 17:46:50 +0900

    factory add dao

--- src/main/java/jaeseong/toby/spring/DAOFactory.java
+++ src/main/java/jaeseong/toby/spring/DAOFactory.java
@@ -2,8 +2,11 @@ package jaeseong.toby.spring;
 
 public class DAOFactory {
 	public UserDAO userDao() {
-		ConnectionMaker cm = new NUserDAO();
-		UserDAO dao = new UserDAO(cm);
-		return dao;
+		return new UserDAO(connectionMaker());
+	}
+	
+	// if we have many dao, we divide method.
+	public ConnectionMaker connectionMaker() {
+		return new NUserDAO();
 	}
 }
--- src/main/java/jaeseong/toby/spring/HomeController.java
+++ src/main/java/jaeseong/toby/spring/HomeController.java
@@ -15,30 +15,23 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) throws ClassNotFoundException, SQLException {
 		
-//		UserDAO dao = new UserDAO();
-//		
-//		User user = new User();
-//		user.setId("himan");
-//		user.setName("jaeseong");
-//		user.setPassword("himan2");
-//		
-//		dao.add(user);
-//		
-//		model.addAttribute("first", user.getId()+" add Success");
-//		
-//		User user2=dao.get(user.getId());
-//		
-//		model.addAttribute("second", user2);
+		UserDAO dao = new DAOFactory().userDao();
+		
+		User user = new User();
+		user.setId("himan2");
+		user.setName("jaeseongeda");
+		user.setPassword("himan3");
+		
+		dao.add(user);
+		
+		model.addAttribute("first", user.getId()+" add Success");
+		
+		User user2=dao.get(user.getId());
+		
+		model.addAttribute("second", user2);
 		
 		return "home";
 	}
 	
-	@RequestMapping(value = "/test", method = RequestMethod.GET)
-	public String test(Model model) throws ClassNotFoundException, SQLException {
-		
-		UserDAO dao = new DAOFactory().userDao();
-		
-		return "home";
-	}
 	
 }
