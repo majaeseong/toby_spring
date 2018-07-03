6860505365d25e15a6da52f03ce94efe35b27351
jaeseong <cpontina@naver.com>
2018-07-03 17:58:41 +0900

    annotationApplicationContext
    ApplicationContext = BeanFactory + added spring service

--- src/main/java/jaeseong/toby/spring/DAOFactory.java
+++ src/main/java/jaeseong/toby/spring/DAOFactory.java
@@ -1,11 +1,18 @@
 package jaeseong.toby.spring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DAOFactory {
+	
+	@Bean
 	public UserDAO userDao() {
 		return new UserDAO(connectionMaker());
 	}
 	
 	// if we have many dao, we divide method.
+	@Bean
 	public ConnectionMaker connectionMaker() {
 		return new NUserDAO();
 	}
--- src/main/java/jaeseong/toby/spring/HomeController.java
+++ src/main/java/jaeseong/toby/spring/HomeController.java
@@ -2,6 +2,8 @@ package jaeseong.toby.spring;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,20 +17,24 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) throws ClassNotFoundException, SQLException {
 		
-		UserDAO dao = new DAOFactory().userDao();
+//		UserDAO dao = new DAOFactory().userDao();
+//		
+//		User user = new User();
+//		user.setId("himan2");
+//		user.setName("jaeseongeda");
+//		user.setPassword("himan3");
+//		
+//		dao.add(user);
+//		
+//		model.addAttribute("first", user.getId()+" add Success");
+//		
+//		User user2=dao.get(user.getId());
+//		
+//		model.addAttribute("second", user2);
 		
-		User user = new User();
-		user.setId("himan2");
-		user.setName("jaeseongeda");
-		user.setPassword("himan3");
-		
-		dao.add(user);
-		
-		model.addAttribute("first", user.getId()+" add Success");
-		
-		User user2=dao.get(user.getId());
-		
-		model.addAttribute("second", user2);
+		ApplicationContext context=
+				new AnnotationConfigApplicationContext(DAOFactory.class);
+		UserDAO dao = context.getBean("userDao", UserDAO.class);
 		
 		return "home";
 	}
